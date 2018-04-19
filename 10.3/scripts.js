e4431a76ecad2775210b9ddd684af28c3081ef63
Danuta <danprzy@gmail.com>
2018-04-19 17:59:20 +0200

    Add next prev buttons

--- 10.3/scripts.js
+++ 10.3/scripts.js
@@ -3,15 +3,29 @@ $(function(){
 	function changeSlide() {
 		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 	}
-	setInterval(changeSlide, 3000); 
+	//setInterval(changeSlide, 7000); 
 
 	function moveFirstSlide() {
-	var firstItem = carouselList.find('li:first');
-	var lastItem = carouselList.find('li:last');
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
+		var firstItem = carouselList.find('li:first'); 
+		var lastItem = carouselList.find('li:last');
+		lastItem.after(firstItem); // za ostatnim elementem dodaj pierwszy
+		carouselList.css({marginLeft:0});
 	}
 
-	//$prev.on('click', scrollPrev);
-	//$next.on('click', scrollNext);
+	//kręcenie pod przycisk w prawo
+	$('#next').on('click', function changeSlideOnClickRight() {
+	carouselList.animate({'marginLeft':-400}, 100, moveFirstSlide);
+	});
+
+	//kręcenie pod przycisk w lewo
+	$('#prev').on('click', function changeSlideOnClickLeft() {
+	carouselList.animate({'marginRight':-400}, 100, moveFirstSlideBack);
+	});
+
+	function moveFirstSlideBack() {
+		var firstItem = carouselList.find('li:first'); 
+		var lastItem = carouselList.find('li:last');
+		firstItem.before(lastItem);// dodawanie przed pierwszym elmentem ostatniego
+		carouselList.css({marginLeft:0});
+	}
 });
