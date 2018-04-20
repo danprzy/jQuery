98f81587dddd17555416938d6fc9ce317ff201a2
Danuta <danprzy@gmail.com>
2018-04-20 12:37:38 +0200

    Add inArray method

--- 10.2/scripts.js
+++ 10.2/scripts.js
@@ -1,16 +1,16 @@
-var span = $('span');
-    span.each(function(index, element) {
-});
-
+//debugger
+var spans = $('span');
 var ignored = ["spanElem0", "spanElem3" ];
 
-span.each(function(index, element) {
-	// if() { ?
-		$(element).css('color', 'red');
-	//};
-});
-	
 
+$('span').each(function(index, element) {
+	if($.inArray(element, ignored) < 0) {
+		$(element).css('color', 'red');
+	};
+});
+
+//jQuery.inArray( value, array [, fromIndex ] )
+//The $.inArray() method is similar to JavaScript's native .indexOf() 
 /*
 span.each(function(index, element) {
 	if(index % 2 == 0) {
@@ -19,6 +19,7 @@ span.each(function(index, element) {
 });
 */
 // $('span:even').css('color', 'red');
+
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
     var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
