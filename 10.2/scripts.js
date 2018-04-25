dd868b09a53a94d3713fb50cbf62dee9ad620095
Danuta <danprzy@gmail.com>
2018-04-25 12:06:09 +0200

    Add indexOf method, remove inArray method

--- 10.2/scripts.js
+++ 10.2/scripts.js
@@ -1,32 +1,33 @@
-//debugger
-var spans = $('span');
-var ignored = ["spanElem0", "spanElem3" ];
+$(function(){
+
+	var ignored = ["spanElem2", "spanElem3" ];
+
+	$('span').each(function(index, element) {
+		var idx = ignored.indexOf(element.id)
+		//console.log(idx);
+		//console.log(element.id);
+			if(idx == -1) { // kiedy elementu nie ma w tablicy ignored to koloruj na czerwono
+				$(element).css('color', 'red');
+			};
+	});
+
+	/* version 1 co drugi element koloruj na czerwono, sprawdzamy czy jest parzysty
+	span.each(function(index, element) {
+		if(index % 2 == 0) { 
+			$(element).css('color', 'red');
+		};
+	});
+	*/
 
 
-$('span').each(function(index, element) {
-	if($.inArray(element, ignored) < 0) {
-		$(element).css('color', 'red');
-	};
+	var paragraphs = $('p');
+	paragraphs.each(function(index, element) {
+	    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+	    $(element).append(button);
+	});
+
+	$('button').click(function(){
+		alert($(this).attr('data-tmp'));
+	});
+
 });
-
-//jQuery.inArray( value, array [, fromIndex ] )
-//The $.inArray() method is similar to JavaScript's native .indexOf() 
-/*
-span.each(function(index, element) {
-	if(index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
-});
-*/
-// $('span:even').css('color', 'red');
-
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-    $(element).append(button);
-});
-
-$('button').click(function(){
-	alert($(this).attr('data-tmp'));
-});
-
