17bbd397f5d24042df997491a63d16a700b6615d
Danuta <danprzy@gmail.com>
2018-05-08 22:28:55 +0200

    Change function names

--- 10.3/scripts.js
+++ 10.3/scripts.js
@@ -1,28 +1,27 @@
-$(function(){
-	var carouselList = $('#carousel ul');
-	function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-	}
-	//setInterval(changeSlide, 7000); 
+$(function () {
+  var carouselList = $('#carousel ul');
+    function changeSlide() {
+        carouselList.animate({'marginLeft': -400}, 1000, moveFirstSlide);
+    }
+    setInterval(changeSlide, 3000); 
 
-	function moveFirstSlide() {
-		var firstItem = carouselList.find('li:first'); 
-		var lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem); // za ostatnim elementem dodaj pierwszy
-		carouselList.css({marginLeft:0}); //zresetuje lewy margines z powrotem na wartość 0
-	}
+    function moveFirstSlide() {
+        var firstItem = carouselList.find('li:first');
+        var lastItem = carouselList.find('li:last');
+        lastItem.after(firstItem); // za ostatnim elementem dodaj pierwszy
+        carouselList.css({marginLeft: 0}); //zresetuje lewy margines z powrotem na wartość 0
+    }
 
-	//kręcenie pod przycisk w prawo
-	$('#next').on('click', function changeSlideOnClickRight() {
-	carouselList.animate({'marginLeft':-400}, 1000, moveFirstSlide);
-	});
+    //kręcenie pod przycisk w prawo
+    $('#next').on('click', function () {
+        carouselList.animate({'marginLeft': -400}, 1000, moveFirstSlide);
+    });
 
-	//kręcenie pod przycisk w lewo
-	$('#prev').on('click', function moveFirstSlideBack() {
-		var firstItem = carouselList.find('li:first'); 
-		var lastItem = carouselList.find('li:last');
-		firstItem.before(lastItem);// dodawanie przed pierwszym elmentem ostatniego
-		carouselList.css({marginLeft: -400}).animate({'marginLeft': 0}, 1000);//zrób animacje od -400 do 0, trwającą 1000ms
-	});
-	
+    //kręcenie pod przycisk w lewo
+    $('#prev').on('click', function () {
+        var firstItem = carouselList.find('li:first');
+        var lastItem = carouselList.find('li:last');
+        firstItem.before(lastItem);// dodawanie przed pierwszym elmentem ostatniego
+        carouselList.css({marginLeft: -400}).animate({'marginLeft': 0}, 1000);//zrób animacje od -400 do 0, trwającą 1000ms
+    });
 });
