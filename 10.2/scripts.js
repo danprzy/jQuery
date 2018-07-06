9e6820ebe027041f8e2a0a6f9e07365aa2df7ce8
Danuta Przydrozna <danprzy@gmail.com>
2018-07-06 16:10:53 +0200

    Add readme prettify code

--- 10.2/scripts.js
+++ 10.2/scripts.js
@@ -1,33 +1,33 @@
-$(function(){
+$(function() {
 
-	var ignored = ["spanElem2", "spanElem3" ];
+    var ignored = ["spanElem2", "spanElem3"];
 
-	$('span').each(function(index, element) {
-		var idx = ignored.indexOf(element.id)
-		//console.log(idx);
-		//console.log(element.id);
-			if(idx == -1) { // kiedy elementu nie ma w tablicy ignored to koloruj na czerwono
-				$(element).css('color', 'red');
-			};
-	});
+    $('span').each(function(index, element) {
+        var idx = ignored.indexOf(element.id)
+        //console.log(idx);
+        //console.log(element.id);
+        if (idx == -1) { // kiedy elementu nie ma w tablicy ignored to koloruj na czerwono
+            $(element).css('color', 'red');
+        };
+    });
 
-	/* version 1 co drugi element koloruj na czerwono, sprawdzamy czy jest parzysty
-	span.each(function(index, element) {
-		if(index % 2 == 0) { 
-			$(element).css('color', 'red');
-		};
-	});
-	*/
+    /* version 1 co drugi element koloruj na czerwono, sprawdzamy czy jest parzysty
+    span.each(function(index, element) {
+    	if(index % 2 == 0) { 
+    		$(element).css('color', 'red');
+    	};
+    });
+    */
 
 
-	var paragraphs = $('p');
-	paragraphs.each(function(index, element) {
-	    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-	    $(element).append(button);
-	});
+    var paragraphs = $('p');
+    paragraphs.each(function(index, element) {
+        var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+        $(element).append(button);
+    });
 
-	$('button').click(function(){
-		alert($(this).attr('data-tmp'));
-	});
+    $('button').click(function() {
+        alert($(this).attr('data-tmp'));
+    });
 
 });
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jQ-exercises
+> Basic jQuery exercises
+*  10.2. Basic operation on DOM through jQuery wrapper using each(), css(), append(), attr() methods.
+*  10.3. Creating carousel using animate(), find(), after(), css() methods.
